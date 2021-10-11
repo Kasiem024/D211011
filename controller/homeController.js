2061682c3e485c8659609570efae29ee84fe347a
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-11 12:34:44 +0200

    experimenting with airtable

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,5 +1,14 @@
 'use strict';
 
 exports.index = (req, res) => {
+
+    const Airtable = require('airtable');
+    const base = new Airtable({ apiKey: 'keyAlLLzNbI6dhsd1' }).base('appoIX6ThocYXfAlx');
+
+    base('Design projects').find('recoXn3k9wwghAGwd', function(err, record) {
+        if (err) { console.error(err); return; }
+        console.log('Retrieved', record.id);
+    });
+
     res.sendfile('public/index.html');
 };
