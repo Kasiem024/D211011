a2093f035f630c5a0a0312a4b84eb62d34b23944
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-11 15:49:24 +0200

    can print to console log

--- controller/homeController.js
+++ controller/homeController.js
@@ -5,10 +5,25 @@ exports.index = (req, res) => {
     const Airtable = require('airtable');
     const base = new Airtable({ apiKey: 'keyAlLLzNbI6dhsd1' }).base('appoIX6ThocYXfAlx');
 
-    base('Design projects').find('recoXn3k9wwghAGwd', function(err, record) {
+    base('Design projects').select({
+        // Selecting the first 3 records in All projects:
+        view: "All projects"
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function(record) {
+            console.log('Retrieved', record.id);
+
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+    }, function done(err) {
         if (err) { console.error(err); return; }
-        console.log('Retrieved', record.id);
     });
 
-    res.sendfile('public/index.html');
+    res.sendfile('public/home.html');
 };
